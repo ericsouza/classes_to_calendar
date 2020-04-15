97824c7db9826ebf95d53d6f255ad3db83207978
Eric Souza <ericsouza0801@gmail.com>
2020-04-15 13:50:46 -0300

    added create classes infos of a student

--- extract_classes.py
+++ extract_classes.py
@@ -1,5 +1,7 @@
 import PyPDF2
 import re
+import os
+import pandas as pd
 
 ra_regex = re.compile(r"^\d{11}$|^\d{8}$")
 turma_regex = re.compile(r".*-\d\d(SA|SB)$")
@@ -7,11 +9,9 @@ turma_regex = re.compile(r".*-\d\d(SA|SB)$")
 def extract_text_from_pdf(input_pdf, output_txt):
     pdf_file = open(input_pdf, 'rb')
     read_pdf = PyPDF2.PdfFileReader(pdf_file)
-
     number_of_pages = read_pdf.getNumPages()
-
+    
     pdf_content = []
-
     for i in range(number_of_pages):
         print(f'appending page {i} of {number_of_pages}')
         page = read_pdf.getPage(i)
@@ -27,17 +27,37 @@ def extract_text_from_pdf(input_pdf, output_txt):
             deferidas.write(page)
             c+=1
 
+def create_classes_dataframe_from_xlsx(xlsx_file):
+    labels = ['Código', 'Disicplina - turma', 'teoria', 'prática', 'docente teoria', 'docente prática']
+    df = pd.read_excel(xlsx_file)
+
+    df.drop(df[(df[labels[0]] == labels[0]) & (df[labels[1]] == labels[1]) & (df[labels[5]] == labels[5])].index, inplace=True)
+    df.reset_index(inplace=True)
+    df = df.drop('index', 1)
+
+    df.loc[df[labels[2]] == 0, labels[2]] = ''
+    df.loc[df[labels[3]] == 0, labels[3]] = ''
+    df.loc[df[labels[4]] == 0, labels[4]] = ''
+    df.loc[df[labels[5]] == 0, labels[5]] = ''
+
+    df = df.fillna('')
+    df.drop(df.columns.difference(labels), 1, inplace=True)
+
+    return df
 
 def get_classes(deferidas_file, ra):
-    
     # ! comentar sobre uso de replace para nomear o arquivo de output txt
-    #extract_text_from_pdf(deferidas_file, deferidas_file.replace('.pdf', '.txt'))
+    if not os.path.isfile(deferidas_file.replace('.pdf', '.txt')):
+        extract_text_from_pdf(deferidas_file, deferidas_file.replace('.pdf', '.txt'))
 
-    cod_turmas_aluno = []
+    cod_turmas_aluno = {
+        'SA': [],
+        'SB': []
+    }
     
     cod_turmas_lista = []
     ra_lista = []
-
+    
     with open(deferidas_file.replace('.pdf', '.txt'), 'r', encoding='utf-8') as file:
         all_lines = file.readlines()
         
@@ -51,19 +71,52 @@ def get_classes(deferidas_file, ra):
     for i in range (len(ra_lista)):
         if ra in ra_lista[i]:
             # !comentar sobre remover \n do final
-            cod_turmas_aluno.append(cod_turmas_lista[i].strip())
+            cod_turma = cod_turmas_lista[i].strip()
+            if cod_turma[-2:] == "SA":
+                cod_turmas_aluno['SA'].append(cod_turma)
+            elif cod_turma[-2:] == "SB":
+                cod_turmas_aluno['SB'].append(cod_turma)
     
-    has_sa = False
-    has_sbc = False
+    if cod_turmas_aluno['SA']:
+        df_sa = create_classes_dataframe_from_xlsx('turmas_sa.xlsx')
+        df_sa.rename(columns={
+            'Código':'codigo', 'Disicplina - turma':'disciplina',
+            'prática':'pratica', 'docente teoria':'docente_teoria',
+            'docente prática':'docente_pratica'    
 
-    for turma in cod_turmas_aluno:
-        if turma[-2:] == "SA":
-            has_sa = True
-        elif turma[-2:] == "SB":
-            has_sbc = True
+        }, inplace=True)
 
-    print(cod_turmas_aluno)
-    print('Santo andré? ', has_sa, '| São Bernardo?', has_sbc)
+        df_sa.set_index("codigo", inplace=True)
 
+    if cod_turmas_aluno['SB']:
+        df_sb = create_classes_dataframe_from_xlsx('turmas_sb.xlsx')
+        df_sb.rename(columns={
+            'Código':'codigo', 'Disicplina - turma':'disciplina',
+            'prática':'pratica', 'docente teoria':'docente_teoria',
+            'docente prática':'docente_pratica'    
 
-get_classes('deferidas.pdf', '11201720512')
+        }, inplace=True) 
+        df_sb.set_index("codigo", inplace=True)
+    
+    aulas = []
+    for turma in cod_turmas_aluno['SA']:
+        try:
+            disc = df_sa.loc[turma].to_dict()
+            disc.update({'campus': 'Santo André'})
+            disc['disciplina'] = disc['disciplina'].replace('(Santo André)', '').strip()
+            aulas.append(disc)
+        except:
+            pass
+    
+    for turma in cod_turmas_aluno['SB']:
+        try:
+            disc = df_sb.loc[turma].to_dict()
+            disc.update({'campus': 'São Bernardo do Campo'})
+            disc['disciplina'] = disc['disciplina'].replace('(São Bernardo do Campo)', '').strip()
+            aulas.append(disc)
+        except:
+            pass
+
+    return aulas
+
+print(get_classes('deferidas.pdf', '11002112'))
